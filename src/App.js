fb381f778ade514620cc67c5cc94a98695d1df72
khaled <khaled@nascenia.com>
2017-07-26 11:50:10 +0600

    added sample app

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject'
+
+class App extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			projects: []
+		}
+	}
+
+	componentWillMount(){
+		this.setState({
+			projects: [
+				{
+					title: 'biz web',
+					category: 'web design'
+				},
+				{
+					title: 'social app',
+					category: 'mobile'
+				}
+			]
+		});
+	}
+
+	handleAddProject(project){
+		let projects = this.state.projects;
+		projects.push(project);
+		this.setState({projects: projects});
+	} 
+
+	render(){
+		let txt = this.props.txt
+		return (
+			<div>
+				<AddProject AddProject={this.handleAddProject.bind(this)} />
+				<Projects projects={this.state.projects} />
+			</div>	
+		)	
+	}
+}
+
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number
+}
+
+export default App;
